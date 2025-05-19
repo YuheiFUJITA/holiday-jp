12933c7c5b6d56d5e46f54111f4736b0bd4aad9c
Yuhei FUJITA <contact@fujita.dev>
2025-05-20 03:43:49 +0900

    fix

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -4,7 +4,7 @@ import csv from "./route/v1/holiday.csv";
 import xml from "./route/v1/holiday.xml";
 import count from "./route/v1/count";
 import { swaggerUI } from "@hono/swagger-ui";
-import { Bindings } from "./bindings";
+import type { Bindings } from "./bindings";
 import { insertHolidays } from "./db/client";
 import { drizzle } from "drizzle-orm/d1";
 import { stringToDate } from "./utils/date";
@@ -17,44 +17,43 @@ app.use("*", cors({ origin: "*" }));
 app.route("/", json);
 app.route("/", csv);
 app.route("/", xml);
-app.route("/", count);
 app.doc("/doc", {
-  openapi: "3.0.0",
-  info: {
-    version: "1.0.0",
-    title: "Japan Holiday API",
-    description:
-      "内閣府（ https://www8.cao.go.jp/chosei/shukujitsu/gaiyou.html ）が公開している祝日一覧を下に、日本の祝日を取得するAPIです。",
-  },
+	openapi: "3.0.0",
+	info: {
+		version: "1.0.0",
+		title: "Japan Holiday API",
+		description:
+			"内閣府（ https://www8.cao.go.jp/chosei/shukujitsu/gaiyou.html ）が公開している祝日一覧を下に、日本の祝日を取得するAPIです。",
+	},
 });
 
 app.get("/ui", swaggerUI({ url: "/v1/doc" }));
 
 const scheduled: ExportedHandlerScheduledHandler<Bindings> = async (_, env) => {
-  const response = await fetch(
-    "https://www8.cao.go.jp/chosei/shukujitsu/syukujitsu.csv"
-  );
-  const buffer = await response.arrayBuffer();
-  const decoder = new TextDecoder("shift-jis");
-  const csv = decoder.decode(buffer);
+	const response = await fetch(
+		"https://www8.cao.go.jp/chosei/shukujitsu/syukujitsu.csv",
+	);
+	const buffer = await response.arrayBuffer();
+	const decoder = new TextDecoder("shift-jis");
+	const csv = decoder.decode(buffer);
 
-  await insertHolidays(
-    drizzle(env.DB),
-    csv
-      .split("\n")
-      .slice(1)
-      .filter((row) => row.trim() !== "")
-      .map((row) => {
-        const [date, name] = row.split(",");
-        return {
-          date: stringToDate(date),
-          name: name.trim(),
-        };
-      })
-  );
+	await insertHolidays(
+		drizzle(env.DB),
+		csv
+			.split("\n")
+			.slice(1)
+			.filter((row) => row.trim() !== "")
+			.map((row) => {
+				const [date, name] = row.split(",");
+				return {
+					date: stringToDate(date),
+					name: name.trim(),
+				};
+			}),
+	);
 };
 
 export default {
-  fetch: app.fetch,
-  scheduled,
+	fetch: app.fetch,
+	scheduled,
 };
